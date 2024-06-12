5d7ba128aa04c4f6caffc186ddde552b9696ed61
David Aerne <meodai@gmail.com>
2024-06-12 17:49:31 +0200

    fix RGB cube

--- site/src/lib/fn/formatCSS.ts
+++
@@ -1,5 +0,0 @@
-import type { ColorCoords } from 'rybitten';
-
-export const formatCSS = (rgb: ColorCoords): string => {
-  return `rgb(${Math.round(rgb[0] * 255)} ${Math.round(rgb[1] * 255)} ${Math.round(rgb[2] * 255)})`;
-};
