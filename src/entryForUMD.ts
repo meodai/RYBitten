b49b75d25ff7844c3a38544d2d7196ed0d1399dd
nbogie <neillbogie@googlemail.com>
2025-11-23 04:27:09 +0000

    surfacing rybitten.cubes in umd output

---
+++ src/entryForUMD.ts
@@ -0,0 +1,4 @@
+import * as cubes from "./cubes.ts";
+
+export * from "./main.ts";
+export { cubes };
--- vite.config.umd.js
+++ vite.config.umd.js
@@ -2,19 +2,19 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    emptyOutDir: false,
-    lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "rybitten",
-      formats: ["umd"],
-      fileName: (format) => `rybitten.${format}.js`,
+    build: {
+        emptyOutDir: false,
+        lib: {
+            entry: resolve(__dirname, "src/entryForUMD.ts"),
+            name: "rybitten",
+            formats: ["umd"],
+            fileName: (format) => `rybitten.${format}.js`,
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: "rybitten.umd.js",
+                exports: "named",
+            },
+        },
     },
-    rollupOptions: {
-      output: {
-        entryFileNames: "rybitten.umd.js",
-        exports: "named",
-      },
-    },
-  },
 });
