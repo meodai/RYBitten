9b9cf7ac28285e32fcef5ed7dd76e5394616aa2c
David Aerne <meodai@gmail.com>
2026-04-22 14:30:31 +0200

    build(vitest): add test config to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -32,4 +32,13 @@ export default defineConfig({
     },
   },
   plugins: [dts()],
+  test: {
+    include: ["src/__tests__/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      include: ["src/main.ts", "src/cubes.ts"],
+      exclude: ["src/__tests__/**", "src/p5-extension.ts", "src/demo.ts"],
+    },
+  },
 });
